fe48a466388c2e49cb66dcafeebb1a3e4d6b7eb7
Vadertin-1104 <ernestinemoreno4@gmail.com>
2020-05-18 18:06:47 -0700

    add/test routes via Postamn

--- controllers/strains.js
+++ controllers/strains.js
@@ -1,25 +1,51 @@
 const express = require('express')
-const Strain = require('../models/strain')
+const Name = require('../models/strain')
 
 const router = express.Router()
 
-// GET /:
-router.get('/', (req, res) => {
-  Strain.find()
+// GET ALL STRAINS
+router.get('/', (req, res, next) => {
+  Name.find()
     .then(strain => { res.json(strain) })
+    .catch(next)
 })
 
 // GET /:strain
 // router.get('/:strain', (req, res) => {
-//   Strain.find({ strain: req.params.strain })
+//   Name.find({ strain: req.params.strain })
 //     .then(strain => { res.json(strain) })
 // })
 
-// GET /:id
+// GET STRAINS BY ID
 router.get('/:id', (req, res, next) => {
   // console.log(req)
-  Strain.findById(req.params.id)
-    .then(strainID => { res.json(strainID) })
+  Name.findById(req.params.id)
+    .then(strain => { res.json(strain) })
+    .catch(next)
+})
+
+// CREATE|ADD NEW STRAIN
+router.post('/', (req, res, next) => {
+  Name.create(req.body)
+    .then((strain) => res.json(strain))
+    .catch(next)
+})
+
+// EDIT STRAIN BY ID
+router.put('/:id', (req, res, next) => {
+  Name.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true
+  })
+    .then((strain) => res.json(strain))
+    .catch(next)
+})
+
+// DELETE STRAIN BY ID
+router.delete('/:id', (req, res, next) => {
+  Name.findOneAndDelete({
+    _id: req.params.id
+  })
+    .then((strain) => res.json(strain))
     .catch(next)
 })
 
