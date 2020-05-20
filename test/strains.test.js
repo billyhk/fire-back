8bbe887c4fe22b06716ac757f714c8df0f38e47c
Vadertin-1104 <ernestinemoreno4@gmail.com>
2020-05-20 15:25:18 -0700

    GET route tests created and passing

--- test/strains.test.js
+++ test/strains.test.js
@@ -1,9 +1,9 @@
 const should = require('chai').should()
 const expect = require('chai').expect
 const supertest = require('supertest')
-const api = supertest('http://localhost:8000')
+const api = supertest('https://fireinternationalapp.herokuapp.com')
 
-describe('GET/strains', (done) => {
+describe('GET /strains', (done) => {
   it('should return a 200 response', () => {
     api
       .get('/strains')
@@ -21,19 +21,19 @@ describe('GET/strains', (done) => {
   })
 })
 
-describe('GET/strains/:id', (done) => {
+describe('GET /strains/5ec5041b45f19100047b3684', (done) => {
   it('should return a 200 response', () => {
     api
-      .get('/strains/:id')
+      .get('/strains/5ec5041b45f19100047b3684')
       .set('Accept', 'application/json')
       .expect(200, done)
   })
   it('should return an array', (done) => {
     api
-      .get('/strains/:id')
+      .get('/strains/5ec5041b45f19100047b3684')
       .set('Accept', 'application/json')
       .end((error, response) => {
-        expect(response.body).to.be.an('array')
+        expect(response.body).to.be.an('object')
         done()
       })
   })
